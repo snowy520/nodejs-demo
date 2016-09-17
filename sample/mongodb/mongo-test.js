a7abdd7bc14517b799e8fad7bd0a827934f64d3a
xueliming <liming.snowy@gmail.com>
2016-09-17 18:05:10 +0800

    mongo添加bluebird支持

---
+++ sample/mongodb/mongo-test.js
@@ -0,0 +1,8 @@
+/**
+ * Created by xueliming on 9/17/16.
+ */
+var mongoUtil = require('./mongo-util');
+
+mongoUtil.document.findDocument(function(data){
+    console.log(JSON.stringify(data));
+});
---
+++ sample/mongodb/mongo-util.js
@@ -0,0 +1,60 @@
+/**
+ * Created by xueliming on 9/17/16.
+ */
+var MongoDB = require('mongodb'),
+    Promise = require('bluebird'),
+    assert = require('assert');
+
+var MongoClient = MongoDB.MongoClient;
+var url = 'mongodb://localhost:27017/myproject';
+
+function saveDocument(doc) {
+    MongoClient.connect(url, {promiseLibrary:Promise}).then(function(db){
+        var collection = db.collection('documents');
+        collection.find({}).toArray(function(err, docs) {
+            assert.equal(err, null);
+            console.log(JSON.stringify(docs));
+        });
+        return db;
+    }).then(function(db){
+        db.close();
+    }).catch(function(error) {
+        console.error("error", error);
+    });
+}
+
+function findDocument(filter, callback) {
+    if(filter && typeof filter  == 'function') {
+        callback = filter;
+        filter = {};
+    }
+    _operateDb('documents', function(collection) {
+        collection.find(filter).toArray(function(err, docs) {
+            assert.equal(err, null);
+            if(callback && typeof callback == 'function') {
+                callback(docs);
+            }
+        });
+    });
+}
+
+function _operateDb(collection, callback) {
+    var result;
+    MongoClient.connect(url, {promiseLibrary:Promise}).then(function(db){
+        result = callback(db.collection(collection));
+        return db;
+    }).then(function(db){
+        db.close();
+    }).then(function(){
+        return result;
+    }).catch(function(error) {
+        console.error("error", error);
+    });
+}
+
+var document = {
+    saveDocument : saveDocument,
+    findDocument : findDocument
+};
+
+exports.document = document;
